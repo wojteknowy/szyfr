a48fe8459d3349ce7332e55cc27d6bd9f2c250a8
Wojtek <wojteknowy@poczta.fm>
2018-11-10 23:05:44 +0100

    przyciski zwiększają liczby po naciśnieciu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,52 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Label label1;
+    @FXML
+    private Label label2;
+    @FXML
+    private Label label3;
+    @FXML
+    private Label labelAnswer;
+    @FXML
+    private Button buttonAnswer;
+
+    private int number1 = 0;
+    private int number2 = 0;
+    private int number3 = 0;
+    private int count = 3;
+
+    public void checkNumber() {
+        button1.setOnAction(event -> {
+            number1++;
+            label1.setText(Integer.toString(number1));
+        });
+
+        button2.setOnAction(event -> {
+            number2++;
+            label2.setText(Integer.toString(number2));
+        });
+        button3.setOnAction(event -> {
+            number3++;
+            label3.setText(Integer.toString(number3));
+        });
+
+    }
+
+
+    public void initialize() {
+        checkNumber();
+
+    }
 }
